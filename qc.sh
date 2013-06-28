a23d9ec5b715b363cb142c12f5c4427a06d4697e
Daniel Corin <danman4961@gmail.com>
2013-06-28 11:34:43 -0500

    Version 0.2
    First commit. Script takes inline math expression from the command
    line, passes it to Python, and returns an answer to both the console
    and clipboard (clipboard works for mac only as of 6/28)

---
+++ qc.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+VERSION=0.2
+
+if [ $# -lt 1 ] || [ $# -gt 2 ];
+then
+	echo
+	echo qc version $VERSION
+	echo USAGE:
+	echo "qc <expression>"
+	echo "Argument should be surrounded quotes or have no spaces."
+	echo Options:	
+	echo -a: about
+	echo -h: help
+	echo -np: shell with numpy library
+	echo -sp: shell with scipy library
+	echo -s: shell with math library
+	echo -v: version
+	echo
+	exit
+fi
+
+
+case "$1" in
+-a)
+	echo
+	echo About qc:
+	echo
+	echo Written by Daniel Corin
+	echo June 2013
+	echo Special thanks to Dave D\'Alessandro
+	echo
+	exit
+	;;
+-h)	echo	
+	echo "Using qc:"
+	echo "qc passes its input argument directly to the python intepreter with its full math library imported."
+	echo "It uses the python syntax for operators:"
+	echo "Addition: 1+4"
+	echo "Subtract: 3-pi"
+	echo "Multiply: 6*7"
+	echo "Divide: 3.0/8"
+	echo "Powers: 2**10"
+	echo "It will handle simple expression such as 10**8+1 nicely."
+	echo "More complex expression must be passed with quotes: \"exp(2.7)\""
+	echo
+	exit
+	;;
+-s) echo "Opening Python shell with math library..."
+	python -ic "from math import *";
+	exit
+	;;
+-np) echo "Opening Python shell with numpy library..."
+	python -ic "from numpy import *";
+	exit
+	;;
+-sp) echo "Opening Python shell with scipy library..."
+	python -ic "from scipy import *";
+	exit
+	;;
+esac
+
+python -c "from math import *; print $1" | pbcopy
+pbpaste
+
+
